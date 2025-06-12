fe64c1dced0aef2acf416e2747651064af078d7e
Rehpotsirhc <86068565+Rehpotsirhc-z@users.noreply.github.com>
2025-06-12 13:36:53 -0400

    Fix key error

--- frontend/src/pages/RegionView/index.jsx
+++ frontend/src/pages/RegionView/index.jsx
@@ -10,6 +10,7 @@ import {
   Box,
   Divider,
   Chip,
+  ListItem,
   Button,
   TextField,
   LinearProgress,
@@ -30,22 +31,21 @@ import "./RegionView.css";
 import useDataStore from "../../store/DatatableStore.js";
 import useQtlStore from "../../store/QtlStore.js";
 
+// Constants for react-window listbox
 const LISTBOX_PADDING = 8;
 const MAX_VISIBLE = 8;
 const ITEM_SIZE = 36;
 
 function renderRow({ data, index, style }) {
-  const [props, option] = data[index];
+  const item = data[index];
   const inlineStyle = {
     ...style,
     top: style.top + LISTBOX_PADDING,
   };
 
-  return (
-    <Typography component="li" {...props} noWrap style={inlineStyle}>
-      {option}
-    </Typography>
-  );
+  return React.cloneElement(item, {
+    style: inlineStyle,
+  });
 }
 
 const OuterElementContext = React.createContext({});
@@ -228,18 +228,18 @@ function RegionView() {
   const handleGeneChange = (event, newValue) => {
     setSelectedGene(newValue);
     setSelectedSnp("");
-    updateQueryParams(datasetId, newValue, "");
+    updateQueryParams(datasetId, newValue, selectedSnp);
     // TODO clear the graph if no gene is selected?
-    if (newValue != "") fetchSnpData(datasetId, "Astrocytes");
+    if (selectedGene != "") fetchSnpData(datasetId, "Astrocytes");
   };
 
   /** Handles SNP selection change */
   const handleSnpChange = (event, newValue) => {
     setSelectedSnp(newValue);
     setSelectedGene("");
-    updateQueryParams(datasetId, "", newValue);
+    updateQueryParams(datasetId, selectedGene, newValue);
     // TODO clear the graph if no SNP is selected?
-    if (newValue != "") fetchGeneData(datasetId, "Astrocytes");
+    if (selectedSnp != "") fetchGeneData(datasetId, "Astrocytes");
   };
 
   // TODO needed?
@@ -318,7 +318,14 @@ function RegionView() {
                 component: ListboxComponent,
               },
             }}
-            renderOption={(props, option, { index }) => [props, option, index]} // renderOption must return [props, option, index]
+            renderOption={(props, option) => {
+              const { key, ...rest } = props;
+              return (
+                <ListItem key={key} {...rest}>
+                  {option}
+                </ListItem>
+              );
+            }}
             renderInput={(params) => (
               <TextField
                 {...params}
@@ -355,7 +362,14 @@ function RegionView() {
                 component: ListboxComponent,
               },
             }}
-            renderOption={(props, option, { index }) => [props, option, index]} // renderOption must return [props, option, index]
+            renderOption={(props, option) => {
+              const { key, ...rest } = props;
+              return (
+                <ListItem key={key} {...rest}>
+                  {option}
+                </ListItem>
+              );
+            }}
             /* renderTags={(value, getTagProps) => */
             /*   value.map((option, index) => { */
             /*     const { key, ...tagProps } = getTagProps({ index }); */
@@ -397,7 +411,14 @@ function RegionView() {
                 component: ListboxComponent,
               },
             }}
-            renderOption={(props, option, { index }) => [props, option, index]} // renderOption must return [props, option, index]
+            renderOption={(props, option) => {
+              const { key, ...rest } = props;
+              return (
+                <ListItem key={key} {...rest}>
+                  {option}
+                </ListItem>
+              );
+            }}
             /* renderTags={(value, getTagProps) => */
             /*   value.map((option, index) => { */
             /*     const { key, ...tagProps } = getTagProps({ index }); */
