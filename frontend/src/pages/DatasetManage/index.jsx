de8168f3a666c96cd495996878278383e1095afe
RuifengHu <huruifeng.cn@hotmail.com>
2025-08-03 18:11:34 -0400

    Update

--- frontend/src/pages/DatasetManage/index.jsx
+++ frontend/src/pages/DatasetManage/index.jsx
@@ -138,11 +138,20 @@ const DatasetManage = () => {
 
                 {/* Main Content */}
                 <Paper sx={{flex: 1, p: 4}}>
+                    <Typography variant="h6" sx={{mb: 2}}>
+                        <strong>Option 1: Use command line to process dataset</strong>
+                        <br /> 1. Process the dataset using the provided <a href="/help/howtouse?tab=2">scripts </a>(Customize the scripts for your dataset)
+                        <br /> 2. Upload the processed dataset to &quot;backend &gt; datasets&quot; folder,
+                        <br /> 3. Click the &quot;Refresh DB&quot; button
+                    </Typography>
+                    <Divider/>
+                    <Typography variant="h6" sx={{mb: 2}}>
+                        <b> Option 2: Step-by-step process using instructions in the sidebar(UNDER CONSTRUCTION)</b>
+                    </Typography>
                     {getStepContent(activeStep)}
                     <Box sx={{display: 'flex', justifyContent: 'space-between', mt: 2}}>
                         <Button disabled={activeStep === 0} onClick={handleBack}>Back</Button>
-                        <Button variant="contained" onClick={handleNext}
-                                disabled={activeStep >= steps.length || isProcessing}>
+                        <Button variant="contained" onClick={handleNext} disabled={activeStep >= steps.length || isProcessing}>
                             {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
                         </Button>
                     </Box>
